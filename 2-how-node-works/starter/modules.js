8ddd304b32e855c1ce67a8057efbcb1046e726f1
George Gale <gmgale@icloud.com>
2022-08-19 06:39:42 +0200

    module.exports vs exports.

---
+++ 2-how-node-works/starter/modules.js
@@ -0,0 +1,18 @@
+// We can see the args of the parent wrapper function of the module.
+// This shows we are now in a wrapper function.
+console.log(arguments);
+
+// Internally node uses the module 'module' to wrap modules into a func.
+console.log(require('module').wrapper);
+
+// module.exports
+const C = require('./test-module-1');
+const calc1 = new C();
+
+// console.log(calc1.add(3, 7));
+
+// exports
+// - adds properties directly to te exports object
+// We can use ES6 destructing to assign them directly to variables:
+const { add, multiply } = require('./test-module-2');
+console.log(add(3, 7));
