c73ea202717aa128fa3b10c8a5dc9da5a3053f0c
George Gale <gmgale@icloud.com>
2022-08-19 12:15:46 +0200

    Express api with some basic routes.

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     // Express auto sets Content-type: application/json wjem we use .json
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+//
+// app.post('/', (req, res) => {
+//     res
+//         .status(200)
+//         .send("You can post to this endpoint.")
+// })
+
+// Read data before event loop
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //  console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      }); // Created new
+    }
+  );
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
