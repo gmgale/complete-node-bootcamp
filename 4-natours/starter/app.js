26992c0fb7a7da248e2712d926220e6498a6ba93
George Gale <gmgale@icloud.com>
2022-08-20 10:38:50 +0200

    Parameters in get request - returning json data.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -55,6 +55,29 @@ app.post('/api/v1/tours', (req, res) => {
     }
   );
 });
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  // req.params will parse :id into an Object. Can also have /:x/:y/:z that will
+  // be split into the object but are manadtory in the URL and will throw err.
+  // /x? will make it optional. /api/.../:id/:y?/:z?
+  console.log(req.params);
+  const id = req.params.id * 1; // Type coersion string->number
+
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(200).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
