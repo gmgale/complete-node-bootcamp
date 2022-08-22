ad9b0b17beaa9596e6b8d124bba7e8401e5d4550
George Gale <gmgale@icloud.com>
2022-08-22 13:25:57 +0200

    Added middware to validate ID.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -5,6 +5,16 @@ const tours = JSON.parse(
 	fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, val) => {
+	if (req.params.id * 1 > tours.length) {
+		return res.status(404).json({
+			status: "fail",
+			message: "Invalid ID",
+		});
+	}
+	next();
+};
+
 exports.getAllTours = (req, res) => {
 	console.log(req.requestTime);
 	res.status(200).json({
@@ -40,19 +50,7 @@ exports.createTour = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-	// req.params will parse :id into an Object. Can also have /:x/:y/:z that will
-	// be split into the object but are manadtory in the URL and will throw err.
-	// /x? will make it optional. /api/.../:id/:y?/:z?
-	const id = req.params.id * 1; // Type coersion string->number
-
-	const tour = tours.find((el) => el.id === id);
-	// if (id > tours.length) {
-	if (!tour) {
-		return res.status(200).json({
-			status: "fail",
-			message: "Invalid ID",
-		});
-	}
+	const tour = tours[req.params.id];
 	res.status(200).json({
 		status: "success",
 		data: {
@@ -94,12 +92,6 @@ exports.updateWholeTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-	if (req.params.id * 1 > tours.length) {
-		return res.status(404).json({
-			status: "fail",
-			message: "Invalid ID",
-		});
-	}
 	// delete logic goes here...
 	res.status(204).json({
 		status: "success",
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const tourController = require("../controllers/tourController");
 
+// Param middleware (only executes when 'id' is passes in url, and only fir tourRoutes)
+
+// We can use param middleware to check ID to elimenate duplicate code in the handlers
+router.param("id", tourController.checkId);
+
 router
 	.route("/")
 	.get(tourController.getAllTours)
