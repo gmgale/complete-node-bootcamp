9417bf8ebd612587505229875663a7c987385e16
George Gale <gmgale@icloud.com>
2022-08-22 15:58:53 +0200

    Added ESLint and conf.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -7,24 +7,19 @@ const userRouter = require("./routes/userRoutes");
 const app = express();
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV == "development") {
-	app.use(morgan("dev"));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-	console.log("Hello from the middleware 👋");
-	next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.use((req, res, next) => {
-	req.requestTime = new Date().toISOString();
-	next();
-});
-
-	// 3) ROUTES
+// 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
