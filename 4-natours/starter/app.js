2d6589c73fad0d3aaf36b5b44fa891e12c9887c5
George Gale <gmgale@icloud.com>
2022-08-22 15:45:15 +0200

    Env vars for dev/prod.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const morgan = require("morgan");
 
-const app = express();
-
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
-// Middleware to be run on every route
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV == "development") {
+	app.use(morgan("dev"));
+}
+
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
 
-// This is how we would serve static files without a route 
-app.use(express.static(`${__dirname}/public/`));
-
-// Will be called on every request, has to before the routing as the routes end the request/responce cycle.
 app.use((req, res, next) => {
-	console.log("Hello from the middleware!");
+	console.log("Hello from the middleware 👋");
 	next();
 });
 
@@ -24,9 +24,8 @@ app.use((req, res, next) => {
 	next();
 });
 
-// Mounting the routers (middleware to run on specifc routes)
+	// 3) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
-// Exports the app to use in entrypoint server.js
 module.exports = app;
