98575784a8374c0197c5d62a34fca9b8cfb23b15
George Gale <gmgale@icloud.com>
2022-08-23 17:35:30 +0200

    Added createTour to write to DB.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,28 +1,27 @@
 const Tour = require("./../models/tourModels");
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: "400",
-      message: "Missing name or price.",
-    });
-  }
-  next();
-};
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: {
-      tour: "null",
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.errmsg,
+    });
+  }
 };
 
 exports.getTour = (req, res) => {
